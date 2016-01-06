382887f7609585d114d21eac933d36ed0123af4b
Radarn <admin@Arthurs-dator.local>
2016-01-06 12:23:42 +0100

    struggling with sending cards[i] as a parameter

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var cardsFlipped = 0;
 // Fisher Yates Shuffle Modern Algorithm.
 // Watch: https://www.developphp.com/video/JavaScript/Fisher-Yates-Shuffle-Modern-Algorithm-Array-Programming-Tutorial
 // To easier understand exactly how the algorithm works
+window.addEventListener("load", newBoard);
+
 Array.prototype.shuffle = function() {
 
 	var i, j, temp;
@@ -21,29 +23,38 @@ Array.prototype.shuffle = function() {
 	return this;
 }
 
-
-
+/*document.getElementById("memory_board").innerHTML = output;*/
+/*output += '<div id="ID_'+i+'" onclick="flipCards(this,\''+cards[i] + '\')"></div>';*/
 function newBoard() {
 	var div;
 	var output = "";
+	var letter;
 	cardsFlipped = 0;
 	cards.shuffle();
+	document.getElementById("memory_board");
 	for (var i = 0; i < cards.length; i++) {
+		output = cards[i];	
+		alert(output);
 		div = document.createElement("div");
 		div.id = "id_" + i;
-		div.innerHTML = cards[i];
-		output += div
 		div.addEventListener("click", function() {
-			flipCards(this, cards[i]);
+			flipCards(this, output);
 		});
-	document.getElementById("memory_board").innerHTML = output;
-
+		memory_board.appendChild(div);
 	}
 
 }
 
-function flipCards() {
-	
-	
+function flipCards(e, val) {
+	alert(val);
+	if (e === "" && cardValues.length < 2) {
+		e.style.background = "#FFF";
+		e.innerHTML = val;
+		if (cardValues.length === 0) {
+			cardValues.push(val);
+			cardIDs.push(e.id);
+		}
+	}
+
 }
 
