58e5f9681e7fd2e5ac7d36df65ff28766d0ce486
Radarn <admin@Arthurs-dator.local>
2016-01-06 14:20:40 +0100

    managed to find the solution on how to pass a unique index argument in the flipSide() function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var cards = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G", "G", "H", "H", "I", "I", "J", "J", "K", "K", "L", "L"];
-var cardValues = [];
-var cardIDs = [];
+var cardValue = [];
+var cardID = [];
 var cardsFlipped = 0;
 
 // Fisher Yates Shuffle Modern Algorithm.
@@ -23,36 +23,93 @@ Array.prototype.shuffle = function() {
 	return this;
 }
 
-/*document.getElementById("memory_board").innerHTML = output;*/
-/*output += '<div id="ID_'+i+'" onclick="flipCards(this,\''+cards[i] + '\')"></div>';*/
-function newBoard() {
+
+
+/*function newBoard() {
 	var div;
 	var output = "";
-	var letter;
 	cardsFlipped = 0;
 	cards.shuffle();
-	document.getElementById("memory_board");
-	for (var i = 0; i < cards.length; i++) {
-		output = cards[i];	
-		console.log(output);
+	/*document.getElementById("memory_board");*/
+	/*for (var i = 0; i < cards.length; i++) {
+		output += '<div id="ID_'+i+'" onclick="flipCards(this,\''+cards[i] + '\')"></div>';
+		document.getElementById("memory_board").innerHTML = output;*/
+
+		//Can't find out how to pass second parameter in flipCards to be a unique index value.
+		/*output = cards[i];
 		div = document.createElement("div");
 		div.id = "id_" + i;
 		div.addEventListener("click", function() {
 			flipCards(this, output);
 		});
-		memory_board.appendChild(div);
-	}
+		memory_board.appendChild(div);*/
+	/*}
 
+}*/
+
+function newBoard() {
+    var div;
+    var output = "";
+    cardsFlipped = 0;
+    cards.shuffle();
+    document.getElementById("memory_board");
+    for (var i = 0; i < cards.length; i++) {
+             
+        output = cards[i];    
+        console.log(output);
+        div = document.createElement("div");
+        div.id = "id_" + i;
+           
+        (function (_out) {
+           
+            div.addEventListener('click', function(){
+                flipCards(this, _out);
+           });
+       })(output);
+    
+        memory_board.appendChild(div)
+    }
 }
 
+
 function flipCards(e, val) {
-	alert(val);
-	if (e === "" && cardValues.length < 2) {
+	console.log(val);
+	if (cardValue.length < 2) {
+		alert(val);
 		e.style.background = "#FFF";
 		e.innerHTML = val;
-		if (cardValues.length === 0) {
-			cardValues.push(val);
-			cardIDs.push(e.id);
+		if (cardValue.length === 0) {
+			cardValue.push(val);
+			cardID.push(e.id);
+		} else if (cardValue === 1) {
+			cardValue.push(val);
+			cardID.push(e.id);
+			if (cardValue[0] === cardValue[1]) {
+				cardsFlipped += 2;
+
+				cardValue = [];
+				cardID = [];
+
+				if (cardsFlipped === cards.length) {
+					alert("Board cleared... generating new board");
+					document.getElementById("memory_board").innerHTML = "";
+					newBoard();
+				}
+			} else {
+				function hideCards() {
+					var card_1 = document.getElementById(cardID[0]);
+					var card_2 = document.getElementById(cardID[1]);
+					card_1.innerHTML = "";
+					card_1.style.background = "#CCC"
+					card_2.innerHTML = "";
+					card_2.style.background = "#CCC";
+
+					cardValue = [];
+					cardID = [];
+
+				}
+				setTimeout(hideCards, 1000);
+			}
 		}
 	}
 
